f2d51d2d5534f62aedefbe3d009dda0680c2fe44
sactuba <taloamn85@gmail.com>
2021-11-05 18:54:46 +0100

    fonction pour les message d'erreur sous les champs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,10 +14,27 @@ const formData = document.querySelectorAll(".formData");
 const closemodal = document.querySelectorAll("#close-modal");
 const erreur = document.querySelectorAll(".erreur");
 
+//Value des input
+const firstNameInput = document.getElementById("first");
+const lastNameInput = document.getElementById("last");
+const emailInput = document.getElementById("email");
+const birhdateInput = document.getElementById("birthdate");
+
+//Value des input
+const firstNameValue = firstNameInput.value;
+const lastNameValue = lastNameInput.value;
+const emailValue = emailInput.value;
+const birhdateValue = birhdateInput.value;
+
+//Id des span pour affiche les erruer du formulaire
+const firstNameError = document.getElementById("firstNameValidation");
+const lastNameError = document.getElementById("lastNameValidation");
+const emailError = document.getElementById("emailValidation");
+const birhdateError = document.getElementById("birthdateValidation");
+
 // Régle regex
-const regEmail = /^[a-zA-Z0-9_\\.\\+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-\\.]+/;
-const regFirst = /^[A-Za-z0-9_-]{2,30}$/;
-const regLast = /^[A-Za-z0-9_-]{2,30}$/;
+const emailRegex = /^^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const nameRegex = /^[A-Za-z0-9_-]{2,30}$/;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -37,27 +54,42 @@ function closeModal() {
 
 
 // Message d'erreur et de validation
-
-function messageErreur() {
-var x = document.querySelector("erreur").innerText  ;
+const errorsMessage = (idValidation) => {
+  let errorMessage = document.getElementById(idValidation);
+  errorMessage.textContent = "Champ non valide!"; 
+  errorMessage.style.color = "red";
+  errorMessage.style.fontSize = '16px';
 }
 
+const validateMessage = (idValidation) => {
+  let errorMessage = document.getElementById(idValidation);
+  errorMessage.textContent = "Champ validé!"; 
+  errorMessage.style.color = "green";
+  errorMessage.style.fontSize = '16px';
+}
+
+
+// fonction de validation
+const validationInput = (regex, value, idValidation) => {
+  if(regex.test(value))
+  {
+  validateMessage(idValidation);     
+  return true;
+  } 
+  else
+  {
+    errorsMessage(idValidation) ; 
+  return false;
+  }
+}
+//Validation nom 
+const firstNameValidation = validationInput(nameRegex, firstNameValue, firstNameError);
+firstNameInput.addEventListener('blur', firstNameValidation); 
+
+//Validation prenom
+const lastNameValidation = validationInput(nameRegex, lastNameValue, lastNameError);
+lastNameInput.addEventListener('blur', lastNameValidation); 
 
 //validation mail
-function emailValidation(mailValue)
-let mailValue = document.getElementById("email").value;
-{
-if(mailValue.match(regEmail))
-{
-alert("Adresse mail validé!");    
-document.querySelector("email").style.color = green;
-return true;
-}
-else
-{
-alert("Adresse mail non valide!");    
-document.querySelector("email").style.color = red;
-return false;
-}
-}
-
+const emailValidation = validationInput(emailRegex, emailValue, emailError);
+emailInput.addEventListener('blur', emailValidation); 
