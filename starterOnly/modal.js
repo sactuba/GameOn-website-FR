bb61a921c80560acebde08b48cdba55d124c84ec
sactuba <taloamn85@gmail.com>
2021-11-09 21:49:33 +0100

    fix de fonction de devalidation de s input du formulaires

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -19,23 +19,29 @@ const erreur = document.querySelectorAll(".erreur");
 const firstNameInput = document.getElementById("first");
 const lastNameInput = document.getElementById("last");
 const emailInput = document.getElementById("email");
-const birhdateInput = document.getElementById("birthdate");
+const birthdateInput = document.getElementById("birthdate");
+const quantityInput = document.getElementById("quantity");
+const submitInput = document.getElementById("submitId");
 
 //Value des input
-const firstNameValue = firstNameInput.value;
+/* const firstNameValue = firstNameInput.value;
 const lastNameValue = lastNameInput.value;
 const emailValue = emailInput.value;
-const birhdateValue = birhdateInput.value;
+const birthdateValue = birthdateInput.value; */
 
 //Id des span pour affiche les erruer du formulaire
 const firstNameError = document.getElementById("firstNameValidation");
 const lastNameError = document.getElementById("lastNameValidation");
 const emailError = document.getElementById("emailValidation");
 const birthdateError = document.getElementById("birthdateValidation");
+const quantityError = document.getElementById("quantityValidation");
+const radioBouttonError = document.getElementById("radioBouttonError");
 
 //Régle regex
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const nameRegex = /^[A-Za-z0-9_-]{2,30}$/;
+const nameRegex = /^[A-Za-z_-]{2,30}$/;
+const birthdateRegex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+const tournoiRegex = /([1-9]?[0-9])|100/;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -53,35 +59,28 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-
-// Message d'erreur et de validation
-/* const errorsMessage = (idValidation) => {
-  let errorMessage = document.getElementById(idValidation);
-  errorMessage.textContent = "Champ non valide!"; 
-  errorMessage.style.color = "red";
-  errorMessage.style.fontSize = '16px';
-}
-
-const validateMessage = (idValidation) => {
-  let errorMessage = document.getElementById(idValidation);
-  errorMessage.textContent = "TESTE"; 
-  errorMessage.style.color = "green";
-  errorMessage.style.fontSize = '16px';
-} */
-
-
 // fonction de validation
-const validationInput = (regex, value, idValidation) => {
-   if(regex.test(value))
+//fonction pour le nom, prenom
+const validationName = (regexName, valueInput, idValidation) => {
+  let value = valueInput.value;
+  let valueLength = value.length;
+   if(regexName.test(value))
   {
-    idValidation.textContent = "Champ validé!"; 
+    idValidation.textContent = "validé!"; 
     idValidation.style.color = "green";
     idValidation.style.fontSize = '16px';    
   return true;
   } 
   else if (value == "")  
   {
-    idValidation.textContent = "Vous devez remplir ce champ!"; 
+    idValidation.textContent = "Ce champ est obligatoire!"; 
+    idValidation.style.color = "red";
+    idValidation.style.fontSize = '16px';
+  return false;
+  } 
+  else if (valueLength <= 2)  
+  {
+    idValidation.textContent = "Veuillez entrer 2 caractères ou plus!"; 
     idValidation.style.color = "red";
     idValidation.style.fontSize = '16px';
   return false;
@@ -95,18 +94,159 @@ const validationInput = (regex, value, idValidation) => {
   }
 }
 
+// Fonction pour l'adresse email
+const validationEmail = (regexName, valueInput, idValidation) => {
+  let value = valueInput.value;
+   if(regexName.test(value))
+  {
+    idValidation.textContent = "validé!"; 
+    idValidation.style.color = "green";
+    idValidation.style.fontSize = '16px';    
+  return true;
+  } 
+  else if (value == "")  
+  {
+    idValidation.textContent = "Ce champ est obligatoire!"; 
+    idValidation.style.color = "red";
+    idValidation.style.fontSize = '16px';
+  return false;
+  } 
+  else
+  {
+    idValidation.textContent = "Champ non valide!"; 
+    idValidation.style.color = "red";
+    idValidation.style.fontSize = '16px';
+  return false;
+  }
+}
+
+// Fonction pour la date de naissance
+const validationBirthdate = (regexName, valueInput, idValidation) => {
+  let value = valueInput.value;
+   if(regexName.test(value))
+  {
+    idValidation.textContent = "validé!"; 
+    idValidation.style.color = "green";
+    idValidation.style.fontSize = '16px';    
+  return true;
+  } 
+  else if (value == "")  
+  {
+    idValidation.textContent = "Ce champ est obligatoire!"; 
+    idValidation.style.color = "red";
+    idValidation.style.fontSize = '16px';
+  return false;
+  } 
+  else
+  {
+    idValidation.textContent = "Champ non valide!"; 
+    idValidation.style.color = "red";
+    idValidation.style.fontSize = '16px';
+  return false;
+  }
+}
+
+//Fonction pour le champ  du nombre de tourner deja fait auparavant
+const validationQuantity = () => {
+  let value = quantityInput.value;
+  if (tournoiRegex.test(value)) 
+  {
+    quantityError.textContent = "validé!"; 
+    quantityError.style.color = "green";
+    quantityError.style.fontSize = '16px';
+    return true;
+  }
+  else if (typeof value == "string")  
+  {
+    quantityError.textContent = "Veuillez saisir un nombre!"; 
+    quantityError.style.color = "red";
+    quantityError.style.fontSize = '16px';
+  return false;
+  }
+  else
+  {
+    quantityError.textContent = "Champ non valide!"; 
+    quantityError.style.color = "red";
+    quantityError.style.fontSize = '16px';
+    return false;
+} 
+}
+
+// Fonction des boutons radio
+/* const valeur; */
+/* function radioBouttonValidation(e){
+  let location = document.getElementsByName('location'),
+      locationError = document.getElementById('radioBouttonError');
+  if (location[0].checked || location[1].checked || location[2].checked || location[3].checked || location[4].checked || location[5].checked){
+    locationError.textContent = "validé!"; 
+    locationError.style.color = "green";
+    locationError.style.fontSize = '16px';
+  } else {
+    e.preventDefault();
+    locationError.textContent = "Cochez une case!"; 
+    locationError.style.color = "red";
+    locationError.style.fontSize = '16px';
+  }
+} */
+const radioBouttonValidation = (e) => {
+  let location = document.getElementsByName('location'),
+      locationError = document.getElementById('radioBouttonError');
+  if (location[0].checked || location[1].checked || location[2].checked || location[3].checked || location[4].checked || location[5].checked){
+    locationError.textContent = "validé!"; 
+    locationError.style.color = "green";
+    locationError.style.fontSize = '16px';
+    return true
+  } else {
+    e.preventDefault();
+    locationError.textContent = "Cochez une case!"; 
+    locationError.style.color = "red";
+    locationError.style.fontSize = '16px';
+    return false
+  }
+}
+// Fonction condition d'utilisation
+const conditionValidation = (e) => {
+  let condition = document.getElementsById('checkbox1'),
+      conditionError = document.getElementById('conditionError');
+  if (condition.checked === true){
+    conditionError.textContent = "validé!"; 
+    conditionError.style.color = "green";
+    conditionError.style.fontSize = '16px';
+    return true
+  } else {
+    e.preventDefault();
+    conditionError.textContent = "Vous devez vérifier que vous acceptez le termes et conditions!"; 
+    conditionError.style.color = "red";
+    conditionError.style.fontSize = '16px';
+    return false
+  }
+}
+
+
+submitInput.addEventListener('click',conditionValidation);
+
+//Validation des bouttons radios
+submitInput.addEventListener('click',radioBouttonValidation);
+
 //Validation nom 
-  firstNameInput.addEventListener('blur', function() {validationInput(nameRegex, firstNameValue, firstNameError);}); 
-/* const  firstNameCheck = validationInput(nameRegex, firstNameValue, firstNameError); */
+firstNameInput.addEventListener('blur', function() {validationName(nameRegex, firstNameInput, firstNameError);});  
+submitInput.addEventListener('click', function() {validationName(nameRegex, firstNameInput, firstNameError);});  
 
 //Validation prenom
-/* const lastNameCheck = validationInput(nameRegex, lastNameValue, lastNameError);*/
-lastNameInput.addEventListener('blur', function() {validationInput(nameRegex, lastNameValue, lastNameError);});  
+lastNameInput.addEventListener('blur', function() {validationName(nameRegex, lastNameInput, lastNameError);});  
+submitInput.addEventListener('click', function() {validationName(nameRegex, lastNameInput, lastNameError);});  
 
-//validation mail
-/* const emailCheck = validationInput(emailRegex, emailValue, emailError);*/
-emailInput.addEventListener('blur', function () {validationInput(emailRegex, emailValue, emailError);}); 
+//validation email
+emailInput.addEventListener('blur', function () {validationEmail(emailRegex, emailInput, emailError);}); 
+submitInput.addEventListener('click', function () {validationEmail(emailRegex, emailInput, emailError);}); 
 
+//validation birthdate
+birthdateInput.addEventListener('blur', function () {validationBirthdate(birthdateRegex, birthdateInput, birthdateError);}); 
+submitInput.addEventListener('click', function () {validationBirthdate(birthdateRegex, birthdateInput, birthdateError);}); 
+
+//validation birthdate
+quantityInput.addEventListener('blur', validationQuantity); 
+submitInput.addEventListener('click', validationQuantity); 
 
 //Fonction pour valider le formulaire
 formId.addEventListener('submit', function(e) {
@@ -114,39 +254,7 @@ formId.addEventListener('submit', function(e) {
 }) ;
 
 
-
-
-/* const firstNameValidation = () => {
-  let idValidation =  firstNameError;
-if(nameRegex.test(firstNameValue))
-{
-  idValidation.textContent = "Champ validé!"; 
-  idValidation.style.color = "green";
-  idValidation.style.fontSize = '16px';    
-return true;
-} 
-else if (firstNameValue == "")  
-{
-  idValidation.textContent = "Vous devez remplir ce champ!"; 
-  idValidation.style.color = "red";
-  idValidation.style.fontSize = '16px';
-return false;
-} 
-else
-{
-  idValidation.textContent = "Champ non valide!"; 
-  idValidation.style.color = "red";
-  idValidation.style.fontSize = '16px';
-return false;
-}
-}
-
-firstNameInput.addEventListener('blur', firstNameValidation);
- */
-
-
-
-//Parcourir les inut du formulaire
+//Parcourir les inut du formulaire !protoype!
 const formTable = formId;
 [...formTable.elements].forEach((input) => {
   console.log(input);
